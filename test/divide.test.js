11a1c4a8bb37fb304eb5d6721cbf7999e24b00e5
Keith Prince <keithcprince@gmail.com>
2019-08-21 11:26:55 -0400

    divide and sort tests

---
+++ test/divide.test.js
@@ -0,0 +1,20 @@
+const {divide} = require('../app');
+const chai = require('chai');
+const expect = chai.expect;
+
+describe("Divide function", () => {
+  it('Should divide positive ints correctly', () => {
+    let a = 8;
+    let b = 4;
+    const expectedAnswer = 2;
+    const c = divide(a,b);
+    expect(c).to.equal(expectedAnswer);
+  });
+  it('Should throw an error when divide by 0', () => {
+    let a = 8;
+    let b = 0;
+    const fn = () => {divide(a,b)};
+
+    expect(fn).to.throw();
+  });
+});
---
+++ test/sort.test.js
@@ -0,0 +1,12 @@
+const {sort} = require('../app');
+const chai = require('chai');
+const expect = chai.expect;
+
+describe("sort function", () => {
+  it('should sort a array of positive numbers', () => {
+    let list = [1,5,30,15,4];
+    let sorted = [1,4,5,15,30];
+    list = sort(list);
+    expect(list).to.deep.equal(sorted);
+  });
+});
