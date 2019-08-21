f5f94f396c9124acda10a9c606d40c1301c8ac47
Ahmed <ahmed.max7586@gmail.com>
2019-08-21 11:54:03 -0400

    created arrayDiff test

---
+++ test/arrayDiff.test.js
@@ -0,0 +1,15 @@
+const expect = require("chai").expect;
+const diff = require("../arrayDiff");
+
+describe("arra diff", () => {
+  it("should return a if no common elements", () => {
+    expect(diff([1, 2, 3], [4, 5, 6])).to.eql([1, 2, 3]);
+  });
+  it("should return [] if same", () => {
+    expect(diff([1, 2, 3], [1, 2, 3])).to.eql([]);
+  });
+
+  it("should return correctly if some elements are removed", () => {
+    expect(diff([1, 2, 3], [2])).to.eql([1, 3]);
+  });
+});
